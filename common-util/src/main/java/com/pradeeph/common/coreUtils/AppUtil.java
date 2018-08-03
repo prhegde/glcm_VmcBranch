2524e6f48fd435d2688cb021c5db94bb0d174920
Pradeep Hegde <pradeeph@vmware.com>
2018-08-03 17:36:53 +0530

    All Code changes on GLCM Development branch

---
+++ common-util/src/main/java/com/pradeeph/common/coreUtils/AppUtil.java
@@ -0,0 +1,13 @@
+package com.pradeeph.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppUtil
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

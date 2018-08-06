b324318048721dd5038f74c8f77db2640bcbc51d
Pradeep Hegde <pradeeph@vmware.com>
2018-08-06 14:58:10 +0530

    All Code changes on VMC branch

---
+++ bringup/bringup-app/src/main/java/com/pradeeph/bringup/Application.java
@@ -0,0 +1,13 @@
+package com.pradeeph.bringup;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
